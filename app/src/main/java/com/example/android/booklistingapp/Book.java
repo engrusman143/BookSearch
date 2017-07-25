d483bd8ef37af7127051befd2209bc9f0ed416ff
Muhammad Usman <engrusman143@yahoo.com>
2017-07-25 17:32:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/booklistingapp/Book.java
@@ -0,0 +1,37 @@
+package com.example.android.booklistingapp;
+
+/**
+ * {@link Book} represents a Google Book.
+ * It consist of a title, a subtitle, authors and a infoLink url.
+ */
+
+public class Book {
+
+    private String mTitle;
+    private String mSubtitle;
+    private String mAuthors;
+    private String mUrl;
+
+    public Book(String title, String subtitle, String authors, String url) {
+        mTitle = title;
+        mSubtitle = subtitle;
+        mAuthors = authors;
+        mUrl = url;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getSubtitle() {
+        return mSubtitle;
+    }
+
+    public String getAuthors() {
+        return mAuthors;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+}
